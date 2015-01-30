8e3172bc91b84118f94f02947ee928f458a8e9de
DonaldDisha <dishad@clarkson.edu>
2015-01-30 13:00:30 -0500

    Update lab1.js

--- lab1.js
+++ lab1.js
@@ -1,20 +1,51 @@
 
 var gl;
 var points;
+var index = 0;
+var canvas;
 
 window.onload = function init()
 {
-    var canvas = document.getElementById( "gl-canvas" );
+    canvas = document.getElementById( "gl-canvas" );
     
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    //set sets of vertices i am using
+    var vertices = [
+		vec2( -0.5, -0.0 ),
+        vec2(  0.0,  0.75 ),
+        vec2(  0.5,  0.0 ),
+		vec2(  0.0, -0.75 ),
+		vec2( -0.5, -0.5 ),
+        vec2(  0.0,  0.5 ),
+        vec2(  0.5,  -0.5 ),
+		vec2( -0.5, 0.5 ),
+        vec2(  0.5, 0.5 ),
+        vec2(  0.5, -0.5 ),
+		vec2(  -0.5,  -0.5 )
+		 
+		];		
+	
+	//clicking input reader
+	canvas.addEventListener("mousedown",function(){
+		if(index == 0) {
+		index = 1;
+	}
+		else if(index == 1) {
+		index = 2;
+	} 
+		else if(index == 2) {
+		index = 0;
+	}
+	});
 
 
     //
     //  Configure WebGL
     //
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    gl.clearColor( 0.0, 0.0, 1.0, 1.0 );
     
     //  Load shaders and initialize attribute buffers
     
@@ -22,8 +53,16 @@ window.onload = function init()
     gl.useProgram( program );
     
     // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 
-    // Associate our shader variables with our data buffer
+    // Associate out shader variables with our data buffer
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
 
     render();
 };
@@ -31,4 +70,17 @@ window.onload = function init()
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
+    
+    // shows each shape, and each shape is determined by a click of the mouse
+    if (index == 0){
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+	}
+	else if (index == 1){
+	gl.drawArrays( gl.TRIANGLES, 4, 3);
+	}
+	else if (index == 2){
+	gl.drawArrays( gl.TRIANGLE_FAN, 7, 4 );
+	}
+	window.requestAnimFrame(render);
 }
+
